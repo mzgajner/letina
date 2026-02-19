b29e79370f30ec87742e9ea3dafe54ead5b3df0a
mzgajner <mato@zgajner.com>
2026-02-19 07:23:32 +0100

    Switch to preact to severely reduce size

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useSyncExternalStore } from "react";
+import { useSyncExternalStore } from "preact/compat";
 import "./App.css";
 import MonthGrid from "./MonthGrid";
 import Verdict from "./Verdict";
